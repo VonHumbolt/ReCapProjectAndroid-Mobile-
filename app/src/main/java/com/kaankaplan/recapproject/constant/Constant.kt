6769718ac96cf201b500fc68820b671e8d99bcfa
Kaan Kaplan <79381882+VonHumbolt@users.noreply.github.com>
2021-07-10 22:37:26 +0300

    Update Constant.kt

--- app/src/main/java/com/kaankaplan/recapproject/constant/Constant.kt
+++ app/src/main/java/com/kaankaplan/recapproject/constant/Constant.kt
@@ -2,6 +2,6 @@ package com.kaankaplan.recapproject.constant
 
 class Constant {
     companion object{
-        const val BASE_URL = "https://192.168.1.104:45455/"
+        const val BASE_URL = "https://<YOUR_IPV4_ADDRESS>:<PORT>/"
     }
 }
